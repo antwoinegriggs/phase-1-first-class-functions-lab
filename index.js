419e7e226663f6d4d98b8248e148881cb4be1715
Antwoine Griggs <97367683+antwoinegriggs@users.noreply.github.com>
2023-03-22 23:40:15 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function (array) {
+  return array.slice(0, 2);
+};
+
+const returnLastTwoDrivers = function (array) {
+  return array.slice(-2);
+};
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = function (fareMultiplier) {
+  return function (fare) {
+    return fare * fareMultiplier;
+  };
+};
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = function (array, key) {
+  return key(array);
+};
